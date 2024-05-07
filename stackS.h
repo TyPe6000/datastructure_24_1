9614491d89fc6bd9ac991e907c19039f27cc8be8
TyPe6000 <162706457+TyPe6000@users.noreply.github.com>
2024-05-07 20:44:49 +0900

    Add files via upload

---
+++ stackS.h
@@ -0,0 +1,11 @@
+#pragma once
+#define STACK_SIZE 100
+
+typedef int element; // 스택원소의 자료형을 int로 정의
+element stack[STACK_SIZE];	// 1차원 배열 스택 선언
+
+int isStackEmpty();
+void push(element item);
+element pop();
+element peek();
+void printStack();
